6a6c230373d539213dcbcb58889fbab738d2d8cb
AntonShiryaev <98731055+AntonShiryaev@users.noreply.github.com>
2023-02-27 23:32:20 +0500

    4.6 Подсчет суммы целых цифр или hashcodeACSII символа путем overloaded constractor sumDigits, в цикле while

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main
         Character character = '$';
         char hre = ' '; //по заданию сказано использовать именно метод int из char
         character = hre;
-        System.out.println("Исходный символ: "+character+". Код символа: "+character.hashCode()+". sumDigitsChar: "+sumDigitsChar(character));
+        System.out.println("Исходный символ: "+character+". Код символа: "+character.hashCode()+". sumDigits: "+sumDigits(character));
         System.out.println("Введите целые цифры или один символ ASCII для расчета суммы цифр:");
         while(true){
             try{
@@ -22,7 +22,7 @@ public class Main
                 String str = console.next();
                 if (str.length()==1){
                     Character c = str.charAt(0);
-                    System.out.println("Исходный символ: "+c+". Код символа: "+c.hashCode()+". sumDigitsChar: "+sumDigitsChar(c));
+                    System.out.println("Исходный символ: "+c+". Код символа: "+c.hashCode()+". sumDigits: "+sumDigits(c));
                 }else{
                     System.out.println("Исходное число для расчета: "+Integer.parseInt(str)+". Результат sumDigits: "+sumDigits(Integer.parseInt(str)));
                 }
@@ -42,21 +42,12 @@ public class Main
         }
         return sum;
     }
-    public static Integer sumDigitsChar(Character decNumber){
+    public static Integer sumDigits(Character decNumber){
         int sum=0;
         for (int i=0; i<String.valueOf(decNumber.hashCode()).length();){
             sum += Integer.valueOf(((String.valueOf(decNumber.hashCode())).charAt(i)-'0'));
             i++;
         }
-        /*
-        System.out.println(decNumber); //вывод значения переданного объекта
-        System.out.println(decNumber.getClass().getName()); //вывод названия имени класса объекта
-        System.out.println(decNumber.toString().getClass().getName()); //перевод значения класса в объект строка
-        String str = new String(decNumber.toString()); //для удобства копирование значения строки в новую строку
-        System.out.println("Проверка получения значения строки: "+str);
-        System.out.println("Длина строки: "+str.length());
-        System.out.println("Элемент 1 строки str: "+(str.charAt(0)-'0'));//int значение
-        */
         return sum;
     }
 }
